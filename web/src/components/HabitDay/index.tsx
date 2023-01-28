11cfadef665dcab64ff4e2d080de1942b03eda90
Luan Campos <luancamposproducoes@gmail.com>
2023-01-28 11:52:29 -0300

    create habit list

--- web/src/components/HabitDay/index.tsx
+++ web/src/components/HabitDay/index.tsx
@@ -1,11 +1,10 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx'
 import dayjs from 'dayjs';
 
 import { ProgressBar } from '../ProgressBar';
-import { Check } from 'phosphor-react';
 import '../../lib/dayjs'
+import { HabitsList } from '../HabitsList';
 
 interface HabitDayProps {
   date: Date
@@ -40,21 +39,7 @@ export const HabitDay = ({ completed = 0, amount = 0, date }: HabitDayProps) => 
 
             <ProgressBar progress={completedPercentage} />
 
-            <div className="mt-6 flex flex-col gap-3">
-              <Checkbox.Root className='flex items-center gap-3 group'>
-                <div
-                  className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
-                >
-                  <Checkbox.Indicator>
-                    <Check size={20} className="text-white" />
-                  </Checkbox.Indicator>
-                </div>
-
-                <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                  Dormir Bem
-                </span>
-              </Checkbox.Root>
-            </div>
+            <HabitsList date={date} />
 
             <Popover.Arrow className="fill-zinc-900" height={8} width={16} />
           </Popover.Content>
